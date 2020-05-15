9e1e81f591709fdbc80082e8d32ec91de2edd9fa
Mickaël Allonneau <mickael.allonneau@gmail.com>
2020-05-15 12:23:00 +0200

    refac

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const getCredentials = require('basic-auth');
+const readCredentials = require('basic-auth');
 const serveStatic = require('serve-static');
 
 /*
@@ -6,39 +6,30 @@ const serveStatic = require('serve-static');
  */
 
 const protect = (url, validator, { directory = process.cwd(), realm = 'default-realm', onAuthFailed = null } = {}) => {
-  // checks >>
   if (typeof url !== 'string')
     throw new Error('`url` is not a string');
 
   if (typeof validator !== 'function')
     throw new Error('`validator` is not a function');
-  // << checks
 
   const serve = serveStatic(directory);
 
   return (req, res) => {
-    // If request URL starts with the URL the user wants to restrict access to
-    if (req.url.startsWith(url)) {
-      const credentials = getCredentials(req);
+    if (req.url.startsWith(url)) { // if request URL requires authentication
+      const credentials = readCredentials(req);
 
-      // If no credentials provided or they're not valid
-      if (!credentials || !validator(credentials.name, credentials.pass)) {
-        // Return 401 to ask for auth
+      if (!credentials || !validator(credentials.name, credentials.pass)) { // if credentials are missing / invalid
         res.writeHead(401, { 'WWW-Authenticate': `Basic realm="${realm}"` });
 
-        // Call user's auth failed handler if provided (e.g. to return a custom HTML error page)
-        if (typeof onAuthFailed === 'function')
+        if (typeof onAuthFailed === 'function') // if the user wants to customize the response
           onAuthFailed(res);
-        // Else, respond with a basic error message
         else
-          res.end('401 Unauthorized');
+          res.end('401 Unauthorized'); // otherwise, send a basic error message
 
-        // Don't serve the requested file
-        return;
+        return; // don't serve the requested file
       }
     }
 
-    // Serve the requested file
     serve(req, res, () => {
       res.statusCode = 404;
       res.end('404 Not Found');
